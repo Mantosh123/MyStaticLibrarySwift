69e498dd9a17778815dbd60958cd4a748d045cd3
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-10-12 23:39:10 +0530

    Create Static Library project

---
+++ MyStaticLibrary/MyStaticLibrary/ConcatinateStaticClass.swift
@@ -0,0 +1,14 @@
+//
+//  ConcatinateStaticClass.swift
+//  MyStaticLibrary
+//
+//  Created by Mantosh Kumar on 12/10/24.
+//
+
+import Foundation
+
+public class ConcatinateStaticClass {
+    static public func concatinateString(fValue: String, sValue: String) -> String {
+        return fValue + sValue
+    }
+}
---
+++ MyStaticLibrary/MyStaticLibrary/MyStaticLibrary.swift
@@ -0,0 +1,10 @@
+//
+//  MyStaticLibrary.swift
+//  MyStaticLibrary
+//
+//  Created by Mantosh Kumar on 12/10/24.
+//
+
+class MyStaticLibrary {
+
+}
